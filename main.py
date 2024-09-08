c7cd0262cd9d275ce5a130d6baaba69f664de82a
tmnbnyaega <mogendinyaega9870@gmail.com>
2024-09-08 11:33:17 +0300

    Doing a while loop

---
+++ main.py
@@ -0,0 +1,20 @@
+# This code demonstrates the syntax of a while loop.
+# The while loop repeats the code until a condition is met.
+# In this case, the code will continue until 'i' becomes greater than or equal to 10.
+
+i = 0
+while i < 10:  # The loop runs while 'i' is less than 10.
+    print(i + 1)  # Printing i + 1 to get numbers from 1 to 10.
+    i += 1  # Incrementing i by 1.
+
+# The expected output is:
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
